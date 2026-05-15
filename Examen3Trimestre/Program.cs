32e5c08939380a350876ddbcb212791149087c94
Alumno1 <Alumno1@PROGRESA36>
2026-05-15 09:13:52 +0200

    Ejercicio 1: Clase album implementada

--- Examen3Trimestre/Program.cs
+++ Examen3Trimestre/Program.cs
@@ -1,7 +1,59 @@
-﻿public class Program
+﻿public class Album
 {
+	string titulo;
+	string artista;
+	int anyo;
+	bool disponible;
+
+	public Album(string titulo, string artista, int anyo, bool disponible)
+	{
+		this.titulo = titulo;
+		this.artista = artista;
+		this.anyo = anyo;
+		this.disponible = disponible;
+	}
+
+	public string getTitulo()
+	{
+		return titulo;
+	}
+
+	public void setTitulo(string titulo)
+	{
+		this.titulo = titulo;
+	}
+
+	public string getArtista()
+	{
+		return artista;
+	}
+
+	public void setArtista(string artista)
+	{
+		this.artista = artista;
+	}
+
+	public int getAnyo()
+	{
+		return anyo;
+	}
+
+	public void setAnyo(int anyo)
+	{
+		this.anyo = anyo;
+	}
+
+	public bool isDisponible()
+	{
+		return disponible;
+	}
+
+	public string toString()
+	{
+		return $"Título: {titulo} - Artista: {artista} {anyo}";
+	}
 	public static void Main(string[] args)
 	{
 		Console.WriteLine("Hello, World!");
 	}
-}1
+}
